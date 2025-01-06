d12fe6269a53ef79c71b1f04e48d3df2c9f36c07
Sanchir <115372855+Sanchir01@users.noreply.github.com>
2025-01-06 16:40:43 +0300

    add new mode and new endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,70 @@
-use axum::extract::Query;
-use axum::response::Html;
-use axum::response::IntoResponse;
-use axum::routing::get;
-use axum::Router;
+pub use self::error::{Error, Result};
+use axum::{
+    extract::{Path, Query},
+    response::{Html, IntoResponse},
+    routing::{get, get_service},
+    Router,
+};
+use tower_http::services::ServeDir;
+use tower::ServiceBuilder;
 use serde::Deserialize;
 use std::net::SocketAddr;
 
+mod error;
+mod web;
+
 #[tokio::main]
 async fn main() {
-    let router_hello = Router::new().route("/hello", get(handles_hello));
-
+    // Создаем маршруты
+    let router_hello = Router::new()
+        .merge(routes_hello());
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("=> Listening on {addr}\n");
 
-    println!("=> Listen on {addr}\n");
-
+    // Запуск сервера
     axum::Server::bind(&addr)
         .serve(router_hello.into_make_service())
         .await
-        .unwrap()
+        .unwrap();
 }
+
+// Роуты для /hello
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handles_hello))
+        .route("/hello/:name", get(handler_hello_name))
+}
+
+// fn routes_static() -> Router {
+//     Router::new().nest_service(
+//         "/",
+//         get_service(ServeDir::new("./static"))
+//             .handle_error(|err: std::io::Error| async move {
+//                 (
+//                     axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+//                     format!("Unhandled internal error: {}", err),
+//                 )
+//             }),
+//     )
+// }
+
 #[derive(Debug, Deserialize)]
 struct HelloParams {
     name: Option<String>,
 }
+
+// Обработчик для /hello
 async fn handles_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<22} - handles_hello - {params:?}", "Handler");
 
     let name = params.name.as_deref().unwrap_or("world");
-
-    Html(format!("hello <div>{name}</div>"))
+    Html(format!("<h1>Hello, {name}!</h1>"))
 }
+
+// Обработчик для /hello/:name
+async fn handler_hello_name(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<22} - handler_hello_name - {name:?}", "Handler");
+
+    Html(format!("<h1>Hello, {name}!</h1>"))
+}
+
